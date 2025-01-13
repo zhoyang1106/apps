dcaa43c83afa610ce27f54b1924ef3bec7db9991
Wk-P <weekend2000119@gmail.com>
2025-01-13 11:52:56 +0900

    update weight function

--- manager_server/manager_node/main.py
+++ manager_server/manager_node/main.py
@@ -107,7 +107,9 @@ def update_weights(app, process_time, worker_id):
 
     task_counts = list(app['processing_tasks_sum'].values())
 
-    new_weight = max(0, 1 / (1 + alpha * process_time + task_counts[worker_id] * beta) - app['dynamic_weight'][worker_id])
+    new_weight = max(0.1, app['dynamic_weight'][worker_id] * 0.9 + 
+                 (1 / (1 + alpha * process_time + task_counts[worker_id] * beta)) * 0.1)
+
     app['dynamic_weight'][worker_id] = new_weight
 
     weights = []
